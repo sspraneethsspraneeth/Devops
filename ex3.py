0c97c3b07f0d5754d0861c7aed03710055c2d5a4
sspraneethsspraneeth <136943258+sspraneethsspraneeth@users.noreply.github.com>
2026-02-03 09:51:38 +0530

    final

--- ex3.py
+++ ex3.py
@@ -2,5 +2,5 @@ def greet(name):
     return f"Hello, {name}! Welcome to the Git feature development."
 
 if __name__ == "__main__":
-    print(greet("Praneeth"))
+    print(greet("Praneethhh"))
     print(greet("Devopslab"))
